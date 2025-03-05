5a15b5fb3d5fe4979ce631eacea1cd0ed7031cc2
Akansha Singh <shubhisingh680@gmail.com>
2025-03-05 15:01:29 +0530

    Add changes

--- src/main/java/org/DataProviders/DataProvidersClass.java
+++ src/main/java/org/DataProviders/DataProvidersClass.java
@@ -7,7 +7,7 @@ public class DataProvidersClass {
     @DataProvider(name="loginData")
     public Object[][] dataProv(){
         return new Object[][] {
-                {"Adminm","admin123"},
+                {"Admin","admin123"},
                 {"verma.ayush@ck.com","Ayush@8493"}
         };
     }
